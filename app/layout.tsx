da3f794bb85ad03de2a79147dc73e6cf5a2657e0
Maxime Lefebvre <lefebvremaxime00@gmail.com>
2025-11-27 20:06:39 +0100

    remove footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         >
           <Header />
           {children}
-          <Footer />
+          {/* <Footer /> */}
         </body>
       </html>
     </>
